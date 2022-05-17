426a3f167bad8e7da8c9dbe8d27994f6732893f2
BillConley01 <conleywilliam001@gmail.com>
2022-05-16 20:34:55 -0400

    edits

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-require("dotenv").config({ path: "./config.env" });
 const path = require("path");
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const dal = require("./dal.js");
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT;
+app.use(cors());
+const PORT = process.env.PORT || 5000;
 // used to serve static files from build directory
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
